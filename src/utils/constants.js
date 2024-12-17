54bbde27c98e5b6cd4674ac8a43b112bd84c926a
Josh Levinson <joshua.levinson@gmail.com>
2024-12-16 22:07:22 -0500

    use vercel for callback

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,11 @@
 // client config
 export const CLIENT_ID = "1fd2e136ea1647d288b47d6341238387";
 export const SPOTIFY_USER_SCOPES = "user-read-private";
-export const REDIRECT_URI = "http://localhost:5173/callback";
+export const REDIRECT_URI =
+  "https://spotify-genre-explorer.vercel.app/callback";
+// if local
+// export const REDIRECT_URI =
+//   "http://localhost:5173/callback";
 
 // spotify URLs
 export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
