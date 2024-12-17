cfd7a6d7d5f6e3b531d1b9b0f5a0035e585a160a
Josh Levinson <joshua.levinson@gmail.com>
2024-12-16 22:35:18 -0500

    small style edits

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,9 @@
 import { authCodeUrl } from "../../utils/auth_code_url";
+import "./AuthPage.css";
 
 function AuthPage() {
   return (
-    <div>
+    <div className="auth">
       <h3>You must login through Spotify to use this application</h3>
       <a href={authCodeUrl}>
         <img width="30%" src="sign_in_with_spotify.jpg" />
