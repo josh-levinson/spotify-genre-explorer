b5cb9928d3b1496bb095c9822abe770c3932f6ab
Josh Levinson <joshua.levinson@gmail.com>
2025-03-12 23:39:42 -0400

    closer to getting eula working

--- src/components/Eula/Eula.jsx
+++ src/components/Eula/Eula.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Eula.css";
-import { eulaText } from "./eula.md";
+import Markdown from "react-markdown";
 
 function Eula() {
   const [eulaAccepted, setEulaAccepted] = useState(false);
+  const [eulaMarkdown, setEulaMarkdown] = useState(null);
 
   function handleAccept() {
     setEulaAccepted(true);
     localStorage.setItem("eulaAccepted", true);
   }
 
+  useEffect(() => {
+    async function fetchEulaMarkdown() {
+      try {
+        const response = await fetch("/eula.md");
+
+        if (!response.ok)
+          throw new Error(`Failed to fetch eula markdown: ${response.status}`);
+
+        const text = await response.text();
+        console.log(text);
+
+        setEulaMarkdown(text);
+      } catch (error) {
+        console.error("Error fetching eula markdown:", error);
+      }
+    }
+
+    fetchEulaMarkdown();
+  }, [setEulaMarkdown]);
+
   return (
     <div className="eula">
-      <div className="eulaText">{eulaText}</div>
+      <div className="eulaText">
+        <Markdown>{eulaMarkdown}</Markdown>
+      </div>
       <div className="eulaActions">
         <label className="eula-checkbox">
           <input
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mdPlugin from "vite-plugin-markdown";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: "esnext", //browsers can handle the latest ES features
   },
-  plugins: [react(), mdPlugin({ mode: "html" })],
+  plugins: [react()],
 });
